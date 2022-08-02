4ef3ce90348549be9918e27e1f6d0da1e8bcee00
244sinfall <dimafilin6@icloud.com>
2022-08-03 01:12:23 +0700

    numbers fix

--- src/components/table/navigationButtons.tsx
+++ src/components/table/navigationButtons.tsx
@@ -7,16 +7,24 @@ const NavigationButtons = () => {
     const dispatch = useAppDispatch()
     const [numbers, setNumbers] = useState<number[]>([])
     useEffect(() => {
-        if(state.content.currentPage < 3) {
-            setNumbers([1, 2, 3, 4, 5])
-        } else if (state.content.maxPage - state.content.currentPage < 3) {
-            let m = state.content.maxPage
-            setNumbers([m-4, m-3, m-2, m-1, m])
-        } else {
-            let c = state.content.currentPage
-            setNumbers([c-2, c-1, c, c+1, c+2])
+        if(state.content.maxPage >= 5){
+            if(state.content.currentPage < 3) {
+                setNumbers([1, 2, 3, 4, 5])
+            } else if (state.content.maxPage - state.content.currentPage < 3) {
+                let m = state.content.maxPage
+                setNumbers([m-4, m-3, m-2, m-1, m])
+            } else {
+                let c = state.content.currentPage
+                setNumbers([c-2, c-1, c, c+1, c+2])
+            }
         }
-
+        else {
+            let numbers: number[] = []
+            for (let i = 1; i <= state.content.maxPage; i++) {
+                numbers.push(i)
+            }
+            setNumbers(numbers)
+        }
     }, [state.content.currentPage, state.content.maxPage])
     return (
         <div className="navigationButtons">
